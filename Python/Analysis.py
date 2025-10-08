b6c3623c7b213b947092393b901afcb00a55ca41
fjr422 <fjr422@alumni.ku.dk>
2025-10-08 13:37:05 +0200

    Update

--- Python/Analysis.py
+++ Python/Analysis.py
@@ -2,7 +2,15 @@ import polars as pl
 import DataPaths
 
 farma_french_paths = DataPaths.FarmaFrench()
-print(farma_french_paths.american_6)
-american_6 = pl.read_csv(farma_french_paths.american_6, separator=',', decimal_comma=False, has_header=True)
 
+ff_research_factors = pl.read_csv(farma_french_paths.ff_research_factors, has_header=True, schema={
+    "DateId": pl.String,
+    "Mkt_RF": pl.Float64,
+    "SMB": pl.Float64,
+    "HML": pl.Float64,
+    "RF": pl.Float64
+})
+print(ff_research_factors.head())
+first = ff_research_factors.with_columns((pl.col("Mkt_RF") + pl.col("SMB") + pl.col("HML")).alias("Test"))
+print(first.head())
 t = 1
--- Python/DataPaths.py
+++ Python/DataPaths.py
@@ -2,3 +2,4 @@ import os
 
 class FarmaFrench:
     american_6 = os.path.join("Data", "6_Portfolios_ME_Prior_12_2.csv")
+    ff_research_factors = os.path.join("Data", "F-F_Research_Data_Factors.csv")
