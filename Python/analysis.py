94106100157162620d5c9cbed4b10feb06397fba
fjr422 <fjr422@alumni.ku.dk>
2025-10-12 13:14:18 +0200

    # Conflicts: #	.gitignore

--- Python/analysis.py
+++ Python/analysis.py
@@ -1,18 +1,18 @@
-import polars as pl
-import polars.selectors as cs
-import plotly
-import plotly.express as px
 import data
 
-
-# Path
-farma_french_paths = data.FarmaFrench()
+# Paths
+fama_french_paths = data.FamaFrenchPaths()
+yield_curve_paths = data.YieldCurvePaths()
 
 # Input
-farma_french_input = data.FarmaFrenchInput(farma_french_paths)
+fama_french_input = data.FamaFrenchInput(fama_french_paths)
+yield_curve_current = data.YieldCurveInput(yield_curve_paths.zero_cupon_europe)
+yield_curve_current.yield_curve_params.write_csv(yield_curve_paths.zero_cupon_europe_params)
+spot_rates = yield_curve_current.risk_free_rate(10, 1/12)
+spot_rates.write_csv(yield_curve_paths.spot_rates_path)
 
-# plot
-american_return_plot = px.scatter(farma_french_input.american6, x ="DateID", y ="weighted_return", color="Portfolio")
-european_return_plot = px.scatter(farma_french_input.european6, x ="DateID", y ="weighted_return", color="Portfolio")
-s = 1
-print(farma_french_input.american6.head())
+
+# Portfolio
+# pivot = fama_french_input.fama_french_portfolios.select(pl.exclude(["number_firms", "size"])).pivot("Portfolio", index = ["DateID", "Market", "Portfolio size"], values = "weighted_return").with_columns((pl.col("DateID") + "01").str.to_date("%Y%m%d").alias("DateID")).sort("DateID").with_row_index()
+# test = pivot.rolling(index_column = "index", period="30i").agg([pl.cov("SMALL LoPRIOR", 'ME1 PRIOR2')])
+
